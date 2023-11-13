5e6dbb904c0cc45cef24be8149b5fe6b1ca28686
Uditsurajsingh <chaudhary.uditsurajsingh@gmail.com>
2023-11-13 23:38:42 +0100

    Setup file

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',  # Replace with your package name
+    version='0.1.0',  # Specify your package version
+    packages=find_packages(),  # Automatically discover and include all packages
+    install_requires=[
+        # List your dependencies here
+    ],
+    author='Udit Suraj Singh',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Uditsurajsingh/DSS_Exercise.git',  # Replace with your GitHub repository URL
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        # Add other classifiers as needed
+    ],
+)
